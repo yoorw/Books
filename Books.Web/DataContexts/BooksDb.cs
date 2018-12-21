42c96cee757928a8a464f2b9b9f7a99983215b2a
Ryan Yoo <ryan.yoo@avanade.com>
2018-12-21 05:44:00 -0600

    Add project files.

---
+++ Books.Web/DataContexts/BooksDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Books.Entities;
+
+namespace Books.Web.DataContexts
+{
+    public class BooksDb : DbContext
+    {
+
+        public BooksDb()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
